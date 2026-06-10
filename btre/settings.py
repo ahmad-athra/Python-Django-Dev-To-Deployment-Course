f1cd2cdec96e7bb767d080acfba230699cc4a1df
Ahmad Athra <aabuatherah@nixpend.com>
2026-06-10 15:34:25 +0300

    feat(compose, setup postgres): add compose and setup postgres

--- btre/settings.py
+++ btre/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btre',
+        'USER': 'api',
+        'PASSWORD': 'api',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
