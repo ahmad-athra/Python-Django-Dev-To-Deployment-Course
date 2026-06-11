fb4cbb88ad4c12321112212b0ec37470ff1c66a0
Ahmad Athra <aabuatherah@nixpend.com>
2026-06-11 12:08:51 +0300

    feat(admin): customize models list views

--- listings/admin.py
+++ listings/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published', 'price', 'realtor')
+    search_fields = ('title', 'price', 'description', 'address', 'city', 'state', 'zipcode')
+    list_per_page = 25
+admin.site.register(Listing, ListingAdmin)
